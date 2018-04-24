44801e1becd50dbf58db57b8d23f47325366f720
Halyette <haly.lesane@gmail.com>
2018-04-24 02:35:06 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,14 @@
 def unsafe?(speed)
-  if speed > 40 or 60 <
-    "unsafe"
+  if speed > 40 
+    return "unsafe"
+  
+  if speed <60
+    return "unsafe"
+    
+  else 
+    return "safe"
+    
   end
-
 end
 
 
